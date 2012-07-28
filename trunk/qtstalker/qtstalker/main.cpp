cda790be850e857f1159e322c5239723b5a88d72
lperepol@gmail.com <lperepol@gmail.com@041e4f6e-a17f-4097-b2f1-d06303cabd38>
2012-07-28 20:37:29 +0000

    Initial Import

---
+++ trunk/qtstalker/qtstalker/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: lperepol
+ *
+ * Created on July 28, 2012, 1:35 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
